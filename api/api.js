6719067ef5aa1cd3c9988ebb359e26902ac2435f
Matt Sell <mwsell@gmail.com>
2014-11-16 14:14:05 -0600

    hash the password

--- api/api.js
+++ api/api.js
@@ -1,39 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var User = require('./models/User.js')
 var app = express();
 
 app.use(bodyParser.json());
 
 // setup some middleware to enable CORS
-app.use(function(req,res,next){
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	next();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 })
 
-var User = mongoose.model('User',{
-	email: String,
-	password: String
-});
+app.post('/register', function (req, res) {
+  var user = req.body;
 
+  var newUser = new User.model({
+    email: user.email,
+    password: user.password
+  });
 
-app.post('/register', function(req,res){
-	var user = req.body;
-
-	var newUser = new User({
-		email: user.email,
-		password: user.password
-	});
-
-	newUser.save(function(err){
-	res.status(200).json(newUser);
-	})	
+  newUser.save(function (err) {
+    res.status(200).json(newUser);
+  })
 })
 
 mongoose.connect('mongodb://localhost/mean-example');
 
-var server = app.listen(3000, function(){
-	console.log('api listening on ', server.address().port);
+var server = app.listen(3000, function () {
+  console.log('api listening on ', server.address().port);
 });
