bc89452990dfc1ed5dd0d15bae11a8f74ff865ca
Matt Sell <mwsell@gmail.com>
2014-11-15 20:43:18 -0600

    generated angular app, plug in ui.router

---
+++ app/scripts/app.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by matt on 11/15/14.
+ */
+angular.module('meanApp').config(function($urlRouterProvider, $stateProvider){
+
+  // if none of these states fit, just go home
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('main',{
+      url:'/',
+      templateUrl: '/views/main.html'
+    })
+
+    .state('register',{
+    url:'/register',
+    templateUrl: '/views/register.html'
+  })
+})
---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name meanApp
+ * @description
+ * # meanExample1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('meanApp', ['ui.router']);
